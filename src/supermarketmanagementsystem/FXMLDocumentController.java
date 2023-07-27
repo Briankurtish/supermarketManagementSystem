88b6c83488774793b2af279fc610b79aaf773482
cipher <asaforbrian18@gmail.com>
2023-07-27 21:33:21 +0300

    added the controller to code

--- src/supermarketmanagementsystem/FXMLDocumentController.java
+++ src/supermarketmanagementsystem/FXMLDocumentController.java
@@ -6,10 +6,13 @@ package supermarketmanagementsystem;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 /**
  *
@@ -17,14 +20,38 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
+     @FXML
+    private AnchorPane admin_form;
+
     @FXML
-    private Label label;
-    
+    private Hyperlink admin_hyperLink;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private Button admin_loginBtn;
+
+    @FXML
+    private PasswordField admin_password;
+
+    @FXML
+    private TextField admin_username;
+
+    @FXML
+    private AnchorPane employee_form;
+
+    @FXML
+    private Hyperlink employee_hyperLink;
+
+    @FXML
+    private TextField employee_id;
+
+    @FXML
+    private Button employee_loginBtn;
+
+    @FXML
+    private PasswordField employee_password;
+
+    @FXML
+    private AnchorPane main_form;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
